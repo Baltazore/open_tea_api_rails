5dd63679366c0a0dc680e620b03fd30ea4df1306
Kirill Shaplyko <k.shaplyko@gmail.com>
2015-04-03 22:18:33 +0300

    Fill controller actions

--- app/controllers/api/teas_controller.rb
+++ app/controllers/api/teas_controller.rb
@@ -6,4 +6,34 @@ class Api::TeasController < ApplicationController
   def show
     render json: Tea.find(params[:id])
   end
+
+  def create
+    @tea = Tea.new tea_params
+    if @tea.create
+      render json: @tea
+    else
+      render nothing: true, status: 403
+    end
+  end
+
+  def update
+    @tea = Tea.find(params[:id])
+    @tea.assign_attributes tea_params
+
+    if @tea.update
+      render json: @tea
+    else
+      render json: @tea, status: 403
+    end
+  end
+
+  def destroy
+    @tea = Tea.find(params[:id])
+
+    if @tea.destroy
+      render nothing: true, status: 200
+    else
+      render nothing: true, status: 403
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :teas
+    resources :teas, except: [:new, :edit]
   end
 end
