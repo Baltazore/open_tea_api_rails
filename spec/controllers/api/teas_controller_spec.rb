8c1520825d15d01474520a8187f5a08d7f932cff
Kirill Shaplyko <k.shaplyko@gmail.com>
2015-04-04 15:46:54 +0300

    Add show test

--- spec/controllers/api/teas_controller_spec.rb
+++ spec/controllers/api/teas_controller_spec.rb
@@ -2,18 +2,37 @@ require 'rails_helper'
 
 RSpec.describe Api::TeasController, type: :controller do
 
+  let!(:tea) { Tea.create(name: 'tea', description: 'awesome tea') }
+
+  after(:each) do
+    Tea.destroy_all
+  end
+
   it 'renders all created teas' do
-    tea = Tea.create(name: 'tea', description: 'awesome tea')
+    # Get index action
     get :index
 
+    # Check status code and type of response
     expect(response.status).to eq(200)
     expect(response.content_type).to eq(Mime::JSON)
 
+    # Parse body and checking received object
     teas = JSON.parse(response.body, symbolize_names: true).fetch(:teas)
     expect(teas.count).to eq(Tea.count)
     expect(teas).to include(TeaSerializer.new(tea).attributes)
+  end
 
-    tea.destroy
+  it 'returns requsted tea' do
+    # Get show action
+    get :show, id: tea.id
+
+    # Check status code and type of response
+    expect(response.status).to eq(200)
+    expect(response.content_type).to eq(Mime::JSON)
+
+    # Parse body and checking received object
+    teas = JSON.parse(response.body, symbolize_names: true).fetch(:tea)
+    expect(teas).to eq(TeaSerializer.new(tea).attributes)
   end
 
 end
