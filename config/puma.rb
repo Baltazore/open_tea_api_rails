f5f087902c8258f9a154c6b1ecd17c1237bda91b
Kirill Shaplyko <k.shaplyko@gmail.com>
2015-04-18 18:05:53 +0300

    Changed config

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,7 @@ workers 2
 threads 1, 6
 
 app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+shared_dir = "#{app_dir}/tmp"
 
 # Default to production
 rails_env = ENV['RAILS_ENV'] || "production"
@@ -15,7 +15,7 @@ environment rails_env
 bind "unix://#{shared_dir}/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
 
 # Set master PID and state locations
 pidfile "#{shared_dir}/pids/puma.pid"
